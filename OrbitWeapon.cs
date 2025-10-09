79b956f01f65fd0069557eeb9c3748234704db57
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-10-09 16:45:40 +1100

    Add files via upload

--- OrbitWeapon.cs
+++ OrbitWeapon.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 
-public class OrbitWeapon : MonoBehaviour
+public class OrbitWeapon : Weapon
 {
     [SerializeField] private float orbitSpeed = 10f;
     [SerializeField] private Transform weaponHolder;
     [SerializeField] private Transform weaponToSpawn;
     [SerializeField] private float timeBetweenSpawns = 1f;
+    [SerializeField] private DamageEnemy damageEnemy;
 
     private float spawnTimer;
 
+    private void Start()
+    {
+        SetStats();
+    }
+
     private void Update()
     {
-        weaponHolder.transform.rotation = Quaternion.Euler(0f, 0f, weaponHolder.transform.rotation.eulerAngles.z + orbitSpeed * Time.deltaTime);
+        weaponHolder.transform.rotation = Quaternion.Euler(0f, 0f, weaponHolder.transform.rotation.eulerAngles.z + orbitSpeed * Time.deltaTime * weaponStats[weaponLevel].projectileSpeed);
 
         spawnTimer -= Time.deltaTime;
 
@@ -20,5 +26,21 @@ public class OrbitWeapon : MonoBehaviour
             Instantiate(weaponToSpawn, weaponToSpawn.position, weaponToSpawn.rotation, weaponHolder).gameObject.SetActive(true);
             spawnTimer = timeBetweenSpawns;
         }
+
+        if(statsUpdated)
+        {
+            statsUpdated = false;
+            SetStats();
+        }
+    }
+
+    public void SetStats()
+    {
+        damageEnemy.damageAmount = weaponStats[weaponLevel].damage;
+        transform.localScale = Vector3.one * weaponStats[weaponLevel].range;
+        timeBetweenSpawns = weaponStats[weaponLevel].attackSpeed;
+        damageEnemy.lifetime = weaponStats[weaponLevel].duration;
+
+        spawnTimer = 0f;
     }
 }
