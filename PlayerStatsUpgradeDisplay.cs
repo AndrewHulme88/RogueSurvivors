8c77acf4ae0aa26d6522e4a2c0aef80582021bb6
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-10-12 19:06:15 +1100

    Add files via upload

---
+++ PlayerStatsUpgradeDisplay.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerStatsUpgradeDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text valueText;
+    [SerializeField] private TMP_Text costText;
+    [SerializeField] private GameObject upgradeButton;
+}
