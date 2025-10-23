a3c79c604c750af60c502327703398e405f7f89d
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2025-10-23 20:30:52 +1100

    Add files via upload

--- Projectile.cs
+++ Projectile.cs
@@ -3,9 +3,30 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public bool isBouncingProjectile = false;
+    public bool doesPassThroughEnemies = false;
+    public int remainingBounces = 1;
 
     private void Update()
     {
         transform.position += transform.up * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(isBouncingProjectile && remainingBounces > 0)
+        {
+            if (!doesPassThroughEnemies && collision.CompareTag("Enemy"))
+            {
+                float randomAngle = Random.Range(0f, 360f);
+                transform.rotation = Quaternion.Euler(0f, 0f, randomAngle);
+            }
+
+            remainingBounces--;
+        }
+        else if(isBouncingProjectile && remainingBounces <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
